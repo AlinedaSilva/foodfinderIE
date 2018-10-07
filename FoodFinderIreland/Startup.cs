3fd1cf2dd535dc061544d5ffad83aa0f61b99e88
AlinedaSilva <35935280+AlinedaSilva@users.noreply.github.com>
2018-10-07 09:37:50 +0100

    first commit

---
+++ FoodFinderIreland/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoodFinderIreland.Startup))]
+namespace FoodFinderIreland
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
